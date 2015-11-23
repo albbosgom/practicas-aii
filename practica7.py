33814c4bb6546ab13a90e8f398e11e69af3e3118
albbosgom <albbosgom@alum.us.es>
2015-11-23 16:38:36 +0100

    Terminado el b de la práctica.

--- practica7.py
+++ practica7.py
@@ -13,6 +13,7 @@ def parseMail(mail):
 def app():
     top = Tkinter.Tk()
     top.wm_title("Buscador de correos")
+    
     def createSchema(var):
         schema = Schema(mailFrom=TEXT(stored=True), mailTo=KEYWORD(stored=True), date=DATETIME(stored=True), subject=TEXT(stored=True), content=TEXT(stored=True))
         ix = create_in("Correos", schema)
@@ -90,6 +91,68 @@ def app():
         ctrl.bind("<Return>", lambda(x): createSchema(var.get()))
         ctrl.pack(fill=Tkinter.X)
         panel.mainloop()
+    
+    def buscarRemitentesAsuntos(var):
+        schema = Schema(mailFrom=TEXT(stored=True), mailTo=KEYWORD(stored=True), date=DATETIME(stored=True), subject=TEXT(stored=True), content=TEXT(stored=True))
+        ix = create_in("Correos", schema)
+        writer = ix.writer()
+        files = os.listdir("Correos")
+        for archivo in files:
+            if archivo.endswith(".txt"):
+                f = open("Correos/"+archivo, "r")
+                writer.add_document(mailFrom=unicode(parseMail(f.readline())), mailTo=unicode(parseMail(f.readline())),
+                                    date= unicode(f.readline()), subject= unicode(f.readline()),
+                                    content=unicode(f.readlines()))
+                f.close()
+        writer.commit()
+        
+        schema2 = Schema(mail=TEXT(stored=True), name=TEXT(stored=True))
+        ix2 = create_in("Agenda", schema2)
+        writer2 = ix2.writer()
+        files = os.listdir("Agenda")
+        for archivo in files:
+            if archivo.endswith(".txt"):
+                f = open("Agenda/"+archivo, "r")
+                f2 = f.read()
+                lines = f2.splitlines()
+                mail = ""
+                name = ""
+                for i, line in enumerate(lines):
+                    if i%2 == 0:
+                        mail = parseMail(line)
+                    else:
+                        name = line
+                        writer2.add_document(mail=unicode(mail), name=unicode(name))
+                f.close()
+        writer2.commit()
+        
+        
+        with ix.searcher() as searcher:
+            query = QueryParser("content", ix.schema).parse(unicode(var))
+            results = searcher.search(query)
+            panel = Tkinter.Toplevel()
+            scrollbar = Tkinter.Scrollbar(panel)
+            scrollbar.pack(side = Tkinter.RIGHT, fill=Tkinter.Y)
+            listado = Tkinter.Text(panel, width=150, height=30, yscrollcommand = scrollbar.set)
+            for result in results:
+                listado.insert(Tkinter.INSERT, "Subject: "+result["subject"])
+                
+                with ix2.searcher() as s:
+                    q = QueryParser("mail", ix2.schema).parse(result["mailFrom"])
+                    names = s.search(q)
+                    listado.insert(Tkinter.INSERT, "Mail from: "+names[0]["name"]+"\n\n")
+            scrollbar.config( command = listado.yview )  
+            listado.pack()  
+    
+    def buscarPorCuerpo():
+        panel = Tkinter.Toplevel()
+        lbl = Tkinter.Label(panel, text="Introduzca su query a mirar en el cuerpo del correo: ")
+        lbl.pack(fill=Tkinter.X)
+        var = Tkinter.StringVar()
+        ctrl = Tkinter.Entry(panel, textvariable=var)
+        ctrl.bind("<Return>", lambda(x): buscarRemitentesAsuntos(var.get()))
+        ctrl.pack(fill=Tkinter.X)
+        panel.mainloop()
 
 
     def searchSubject(var):
@@ -131,6 +194,7 @@ def app():
 
     remitente = Tkinter.Button(top, text ="Buscar correos por remitente", command = buscarPorRemitente)    
     remitente.pack(fill=Tkinter.X)
+    
     def buscarPorAsunto():
         panel = Tkinter.Toplevel()
         lbl = Tkinter.Label(panel, text="Introduzca asunto del correo: ")
@@ -140,6 +204,10 @@ def app():
         ctrl.bind("<Return>", lambda(x): searchSubject(var.get()))
         ctrl.pack(fill=Tkinter.X)
         panel.mainloop()
+    
+    prueba = Tkinter.Button(top, text ="Buscar asuntos y remitentes", command = buscarPorCuerpo)    
+    prueba.pack(fill=Tkinter.X)
+    
     asunto = Tkinter.Button(top, text="Buscar correos por asunto", command = buscarPorAsunto)
     asunto.pack(fill=Tkinter.X)
     
