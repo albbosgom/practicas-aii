79b0b0628e9452d7ccdc8da4dcc6a59329139824
feramozur <feramozur@alum.us.es>
2015-12-21 13:48:49 +0100

    Subida la plantilla para la Práctica 10

---
+++ Practica10/mainapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+import mainapp.models as models
+
+admin.site.register(models.Ocupacion)
+admin.site.register(models.Usuario)
+admin.site.register(models.Categoria)
+admin.site.register(models.Pelicula)
+admin.site.register(models.Puntuacion)
---
+++ Practica10/mainapp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Ocupacion(models.Model):
+    nombre = models.CharField(max_length=13)
+
+class Usuario(models.Model):
+    edad = models.SmallIntegerField()
+    sexo = models.CharField(max_length=1)
+    ocupacion = models.ForeignKey(Ocupacion)
+    codigopostal = models.CharField(max_length=5)
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=11)
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=128)
+    fechaestreno = models.DateField()
+    fechavideo = models.DateField(blank=True, null=True)
+    imdburl = models.URLField()
+    categorias = models.ManyToManyField(Categoria)
+
+class Puntuacion(models.Model):
+    usuario = models.ForeignKey(Usuario)
+    pelicula = models.ForeignKey(Pelicula)
+    puntuacion = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    momento = models.DateTimeField()
+    class Meta:
+        unique_together = ("usuario", "pelicula")
